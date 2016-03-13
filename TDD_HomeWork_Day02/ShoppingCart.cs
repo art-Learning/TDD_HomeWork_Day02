ba12c3e007d5fefc4dfceee0bb5cc8b5b79046b5
art <partypeopleland@gmail.com>
2016-03-13 23:00:11 +0800

    Test02_綠燈：第一集買了一本第二集也買了一本價格應為190

--- TDD_HomeWork_Day02/ShoppingCart.cs
+++ TDD_HomeWork_Day02/ShoppingCart.cs
@@ -11,18 +11,30 @@ namespace TDD_HomeWork_Day02
     /// </summary>
     public class ShoppingCart
     {
-        private int 總金額;
+        private double discount;
+        private double 總金額;
         /// <summary>
         /// 結帳計算總金額
         /// </summary>
         /// <param name="books">要購買的書籍</param>
         /// <returns></returns>
-        public int Pay(List<Book> books)
+        public double Pay(List<Book> books)
         {
+            //取得書籍優惠折扣
+            var bookTypeCount = books.Distinct().Count();
+            switch (bookTypeCount)
+            {
+                case 2:
+                    discount = 0.95;
+                    break;
+                default:
+                    discount = 1;
+                    break;
+            }
             //價格加總
             foreach (var book in books)
             {
-                總金額 += book.價格;
+                總金額 += (book.價格*discount);
             }
             return 總金額;
         }
