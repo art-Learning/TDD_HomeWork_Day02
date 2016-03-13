717bd7ca974bb59607b5f5594197cec903e918cf
art <partypeopleland@gmail.com>
2016-03-13 22:46:25 +0800

    Test01_綠燈：第一集買了一本其他都沒買價格應為100元

---
+++ TDD_HomeWork_Day02/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace TDD_HomeWork_Day02
+{
+    public class Book
+    {
+        /// <summary>
+        /// 書籍價格
+        /// </summary>
+        public int 價格 { get; set; }
+        /// <summary>
+        /// 書籍名稱
+        /// </summary>
+        public string 書名 { get; set; }
+        /// <summary>
+        /// 書籍冊數
+        /// </summary>
+        public int 集數 { get; set; }
+    }
+}
--- TDD_HomeWork_Day02/ShoppingCart.cs
+++ TDD_HomeWork_Day02/ShoppingCart.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace TDD_HomeWork_Day02
 {
+    /// <summary>
+    /// 購物車
+    /// </summary>
     public class ShoppingCart
     {
+        private int 總金額;
+        /// <summary>
+        /// 結帳計算總金額
+        /// </summary>
+        /// <param name="books">要購買的書籍</param>
+        /// <returns></returns>
+        public int Pay(List<Book> books)
+        {
+            //價格加總
+            foreach (var book in books)
+            {
+                總金額 += book.價格;
+            }
+            return 總金額;
+        }
     }
 }
