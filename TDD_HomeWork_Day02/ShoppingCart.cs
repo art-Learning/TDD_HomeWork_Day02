c7c55142d790b5c2e52fb961166b7e8cb39e5c13
art <partypeopleland@gmail.com>
2016-03-13 23:52:17 +0800

    Test06_綠燈：一二集各買了一本第三集買了兩本價格應為370

--- TDD_HomeWork_Day02/ShoppingCart.cs
+++ TDD_HomeWork_Day02/ShoppingCart.cs
@@ -20,30 +20,42 @@ namespace TDD_HomeWork_Day02
         /// <returns></returns>
         public double Pay(List<Book> books)
         {
-            //取得書籍優惠折扣
-            var bookTypeCount = books.Distinct().Count();
-            switch (bookTypeCount)
+            while (books.Any())
             {
-                case 2:
-                    discount = 0.95;
-                    break;
-                case 3:
-                    discount = 0.9;
-                    break;
-                case 4:
-                    discount = 0.8;
-                    break;
-                case 5:
-                    discount = 0.75;
-                    break;
-                default:
-                    discount = 1;
-                    break;
-            }
-            //價格加總
-            foreach (var book in books)
-            {
-                總金額 += (book.價格*discount);
+                //取得書籍優惠折扣
+                var bookTypeCount = books.Select(x => x.集數).Distinct().Count();
+                switch (bookTypeCount)
+                {
+                    case 2:
+                        discount = 0.95;
+                        break;
+                    case 3:
+                        discount = 0.9;
+                        break;
+                    case 4:
+                        discount = 0.8;
+                        break;
+                    case 5:
+                        discount = 0.75;
+                        break;
+                    default:
+                        discount = 1;
+                        break;
+                }
+                //這一次要優惠的書
+                List<Book> preferentialBooks = new List<Book>();
+                foreach (var 集數 in books.Select(x => x.集數).Distinct().ToList())
+                {
+                    preferentialBooks.Add(books.Find(x=>x.集數==集數));
+                }
+                    
+                //價格加總
+                foreach (var book in preferentialBooks)
+                {
+                    總金額 += (book.價格 * discount);
+                    //從購物車中把這一次計算優惠的書拿出來
+                    books.Remove(book);
+                }
             }
             return 總金額;
         }
