e345d2d30aebeb8bd61c54dad7caff21bdea24ae
art <partypeopleland@gmail.com>
2016-03-14 00:11:35 +0800

    Test06_重構：一二集各買了一本第三集買了兩本價格應為370

--- TDD_HomeWork_Day02/ShoppingCart.cs
+++ TDD_HomeWork_Day02/ShoppingCart.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TDD_HomeWork_Day02
 {
@@ -11,8 +8,8 @@ namespace TDD_HomeWork_Day02
     /// </summary>
     public class ShoppingCart
     {
-        private double discount;
-        private double 總金額;
+        private double _discount;
+        private double _總金額;
         /// <summary>
         /// 結帳計算總金額
         /// </summary>
@@ -22,42 +19,67 @@ namespace TDD_HomeWork_Day02
         {
             while (books.Any())
             {
-                //取得書籍優惠折扣
-                var bookTypeCount = books.Select(x => x.集數).Distinct().Count();
-                switch (bookTypeCount)
-                {
-                    case 2:
-                        discount = 0.95;
-                        break;
-                    case 3:
-                        discount = 0.9;
-                        break;
-                    case 4:
-                        discount = 0.8;
-                        break;
-                    case 5:
-                        discount = 0.75;
-                        break;
-                    default:
-                        discount = 1;
-                        break;
-                }
-                //這一次要優惠的書
-                List<Book> preferentialBooks = new List<Book>();
-                foreach (var 集數 in books.Select(x => x.集數).Distinct().ToList())
-                {
-                    preferentialBooks.Add(books.Find(x=>x.集數==集數));
-                }
-                    
-                //價格加總
-                foreach (var book in preferentialBooks)
-                {
-                    總金額 += (book.價格 * discount);
-                    //從購物車中把這一次計算優惠的書拿出來
-                    books.Remove(book);
-                }
+                _discount = GetDiscount(books);
+                var preferentialBooks = GetPreferentialBooks(books);
+                GetPrice(books, preferentialBooks, _discount);
             }
-            return 總金額;
+            return _總金額;
+        }
+        /// <summary>
+        /// 計算優惠書籍的價格，並將計算過的書從購物車中移除
+        /// </summary>
+        /// <param name="books">要購買的書籍</param>
+        /// <param name="preferentialBooks">本次優惠書籍</param>
+        /// <param name="disCount">本次優惠折扣</param>
+        private void GetPrice(List<Book> books, List<Book> preferentialBooks,double disCount)
+        {
+            foreach (var book in preferentialBooks)
+            {
+                _總金額 += (book.價格* disCount);
+                books.Remove(book);
+            }
+        }
+        /// <summary>
+        /// 取得符合優惠規則的書籍清單
+        /// </summary>
+        /// <param name="books">要購買的書籍</param>
+        /// <returns></returns>
+        private List<Book> GetPreferentialBooks(List<Book> books)
+        {
+            List<Book> preferentialBooks = new List<Book>();
+            foreach (var 集數 in books.Select(x => x.集數).Distinct().ToList())
+            {
+                preferentialBooks.Add(books.Find(x => x.集數 == 集數));
+            }
+            return preferentialBooks;
+        }
+        /// <summary>
+        /// 取得符合規則的優惠折扣，否則以原價計算
+        /// </summary>
+        /// <param name="books">要購買的書籍</param>
+        /// <returns></returns>
+        private double GetDiscount(List<Book> books)
+        {
+            var bookTypeCount = books.Select(x => x.集數).Distinct().Count();
+            switch (bookTypeCount)
+            {
+                case 2:
+                    _discount = 0.95;
+                    break;
+                case 3:
+                    _discount = 0.9;
+                    break;
+                case 4:
+                    _discount = 0.8;
+                    break;
+                case 5:
+                    _discount = 0.75;
+                    break;
+                default:
+                    _discount = 1;
+                    break;
+            }
+            return _discount;
         }
     }
 }
