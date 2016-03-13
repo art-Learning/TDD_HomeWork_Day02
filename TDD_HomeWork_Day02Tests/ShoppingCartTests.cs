01bc29a4abe56ec43b9159b465d5e0d88953223f
art <partypeopleland@gmail.com>
2016-03-13 23:08:33 +0800

    Test05_紅燈：一次買了整套一二三四五集各買了一本價格應為375

--- TDD_HomeWork_Day02Tests/ShoppingCartTests.cs
+++ TDD_HomeWork_Day02Tests/ShoppingCartTests.cs
@@ -83,6 +83,25 @@ namespace TDD_HomeWork_Day02.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
-
+        [TestMethod()]
+        [TestCategory("哈利波特優惠")]
+        public void 一次買了整套一二三四五集各買了一本價格應為375()
+        {
+            //arrange
+            var books = new List<Book>
+            {
+                new Book {書名 = "哈利波特", 集數 = 1, 價格 = 100},
+                new Book {書名 = "哈利波特", 集數 = 2, 價格 = 100},
+                new Book {書名 = "哈利波特", 集數 = 3, 價格 = 100},
+                new Book {書名 = "哈利波特", 集數 = 4, 價格 = 100},
+                new Book {書名 = "哈利波特", 集數 = 5, 價格 = 100}
+            };
+            var target = new ShoppingCart();
+            var expected = 375;
+            //act
+            var actual = target.Pay(books);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
