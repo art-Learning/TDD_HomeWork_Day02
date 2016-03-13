62f34d79bd6cbcc383ef386ff271f7e7506c11c5
art <partypeopleland@gmail.com>
2016-03-13 22:37:30 +0800

    Test01_紅燈：第一集買了一本其他都沒買價格應為100元

---
+++ TDD_HomeWork_Day02Tests/ShoppingCartTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_HomeWork_Day02;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_HomeWork_Day02.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        [TestCategory("哈利波特優惠")]
+        public void 第一集買了一本其他都沒買價格應為100元()
+        {
+            //arrange
+            var books = new List<Book>
+            {
+                new Book {書名 = "哈利波特", 集數 = 1, 價格 = 100}
+            };
+            var target = new ShoppingCart();
+            var expected = 100;
+            //act
+            var actual = target.Pay(books);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
